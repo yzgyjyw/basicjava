3e90d41521f6868513d34d9cfe28244d490a0f3e
yzgyjyw <yzgyjyw@163.com>
2019-11-04 20:59:17 +0800

    Unsafe deep

---
+++ concurrent/src/main/java/operation/CacheLine.java
@@ -0,0 +1,107 @@
+package operation;
+
+/**
+ * 在计算机系统中,内存是以缓存行为单位进行存储的,缓存行的大小为从32个自己到256个字节不等每一个CPU都有对应的CPU缓存
+ * 伪共享是指不同的线程读到同一个缓存行上的不同的变量
+ */
+public class CacheLine {
+
+    // 测试用的线程数
+    private final static int NUM_THREADS = 4;
+
+    // 测试的次数
+    private final static int NUM_TEST_TIMES = 10;
+
+    // 无填充、无缓存行对齐的对象类
+    static class PlainHotVariable {
+
+        public volatile long value = 0L;
+    }
+
+    // 有填充、有缓存行对齐的对象类, 对象头占用8个字节,再加上value8个字节以及6个用于填充的long类型的对象的48个字节,正好64个字节
+    // 这些字段是有可能在同一个缓存行的
+    static final class AlignHotVariable extends PlainHotVariable {
+
+        public long p1, p2, p3, p4, p5, p6;
+    }
+
+    static final class CompetitorThread extends Thread {
+
+        private final static long ITERATIONS = 500L * 1000L * 1000L;
+
+        private PlainHotVariable plainHotVariable;
+
+        public CompetitorThread(final PlainHotVariable plainHotVariable) {
+            this.plainHotVariable = plainHotVariable;
+        }
+
+        @Override
+        public void run() {
+            // 一个线程对一个变量进行大量的存取操作
+            for (int i = 0; i < ITERATIONS; i++) {
+                plainHotVariable.value = i;
+            }
+
+        }
+
+    }
+
+    public static long runOneTest(PlainHotVariable[] plainHotVariables) throws Exception {
+        // 开启多个线程进行测试
+        CompetitorThread[] competitorThreads = new CompetitorThread[plainHotVariables.length];
+        for (int i = 0; i < plainHotVariables.length; i++) {
+            competitorThreads[i] = new CompetitorThread(plainHotVariables[i]);
+        }
+
+        final long start = System.nanoTime();
+        for (Thread t : competitorThreads) {
+            t.start();
+        }
+
+        for (Thread t : competitorThreads) {
+            t.join();
+        }
+
+        // 统计每次测试使用的时间
+        return System.nanoTime() - start;
+    }
+
+    public static boolean runOneCompare(int theadNum) throws Exception {
+        // 进行无填充、无缓存行对齐的测试
+        PlainHotVariable[] plainHotVariables = new PlainHotVariable[theadNum];
+        for (int i = 0; i < theadNum; i++) {
+            plainHotVariables[i] = new PlainHotVariable();
+        }
+        long t1 = runOneTest(plainHotVariables);
+
+        // 进行有缓冲有缓存的对齐测试
+        AlignHotVariable[] alignHotVariable = new AlignHotVariable[theadNum];
+        for (int i = 0; i < NUM_THREADS; i++) {
+            alignHotVariable[i] = new AlignHotVariable();
+        }
+        long t2 = runOneTest(alignHotVariable);
+
+        System.out.println("Plain: " + t1);
+        System.out.println("Align: " + t2);
+
+        // 返回对比结果
+        return t1 > t2;
+    }
+
+    public static void runOneSuit(int threadsNum, int testNum) throws Exception {
+        int expectedCount = 0;
+        for (int i = 0; i < testNum; i++) {
+            if (runOneCompare(threadsNum))
+                expectedCount++;
+        }
+
+        // 计算有填充、有缓存行对齐的测试场景下响应时间更短的情况的概率
+        System.out.println("Radio (Plain < Align) : " + expectedCount * 100D / testNum + "%");
+    }
+
+    public static void main(String[] args) throws Exception {
+        runOneSuit(NUM_THREADS, NUM_TEST_TIMES);
+    }
+
+
+}
---
+++ concurrent/src/main/java/unsafe/UnSafeDemo.java
@@ -0,0 +1,110 @@
+package unsafe;
+
+import sun.misc.Unsafe;
+
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.stream.IntStream;
+
+public class UnSafeDemo {
+
+    public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException, InstantiationException {
+        allocateMemory();
+    }
+
+    public  static void likeReflect() throws NoSuchFieldException, InstantiationException, IllegalAccessException {
+        /*//由于Unsafe仅供jdk内部类使用,通过此种方式会抛出异常java.lang.SecurityException: Unsafe
+        Unsafe unsafe = Unsafe.getUnsafe();*/
+
+        //通过反射的方式拿到Unsafe
+        Field field = Unsafe.class.getDeclaredField("theUnsafe");
+        field.setAccessible(true);
+        Unsafe unsafe = (Unsafe) field.get(null);
+
+        //使用unsafe初始化类
+        Person person = (Person) unsafe.allocateInstance(Person.class);
+
+        //使用Unsafe实例化类对象的时候，只是给该对象分配内存，不会调用构造方法，因此age为int类型的默认值0
+        System.out.println(person.getAge());
+
+
+        //使用Unsafe修改private字段
+        //这就相当于绕过了set或者构造器中的检查，可以将该值设置成任意的int值
+        Field ageFiled = person.getClass().getDeclaredField("age");
+        unsafe.putInt(person, unsafe.objectFieldOffset(ageFiled), 20);
+        System.out.println(person.getAge());
+    }
+
+    // 抛出checked异常不用trycatch或者在方法声明中抛出
+    public static void throwCheckedException() throws NoSuchFieldException, IllegalAccessException {
+        Field field = Unsafe.class.getDeclaredField("theUnsafe");
+        field.setAccessible(true);
+        Unsafe unsafe = (Unsafe) field.get(null);
+        //使用unsafe抛出的异常可以不用try catch或者在方法声明中throws
+        unsafe.throwException(new IOException());
+    }
+
+    // 声明堆外内存
+    // 注意此时的内存声明是在堆外申请的,不会由GC进行回收,需要自己手动调用freeMemory回收内存
+    public static void allocateMemory() throws NoSuchFieldException, IllegalAccessException {
+        int INT = 10;
+        // 单位字节
+        long size = 4;
+
+
+        Unsafe unsafe;
+        Field f = Unsafe.class.getDeclaredField("theUnsafe");
+        f.setAccessible(true);
+        unsafe = (Unsafe) f.get(null);
+
+        // 申请40个字节的空间
+        long address = unsafe.allocateMemory(size * INT);
+
+        //为40个字节的空间赋值
+        for (int i = 0; i < INT; i++) {
+            unsafe.putInt(address + i * size, i);
+        }
+
+        //取出40个字节的空间的值
+        for (int i = 0; i < INT; i++) {
+            System.out.println(unsafe.getInt(address + i * size));
+        }
+
+        //最后记得释放申请到的内存
+        unsafe.freeMemory(address);
+
+        System.out.println("after free ...");
+        //取出40个字节的空间的值
+        for (int i = 0; i < INT; i++) {
+            System.out.println(unsafe.getInt(address + i * size));
+            //0 0 2 3 4 5 6 7 8 9 释放后内存的值,类似于C语言free后,内存中的是随机的值
+        }
+    }
+
+    // park和unpark
+    /**
+     * JVM在上下文切换的时候使用了Unsafe中的两个非常牛逼的方法park()和unpark()。
+     * 当一个线程正在等待某个操作时，JVM调用Unsafe的park()方法来阻塞此线程。
+     * 当阻塞中的线程需要再次运行时，JVM调用Unsafe的unpark()方法来唤醒此线程。
+     * 我们之前在分析java中的集合时看到了大量的LockSupport.park()/unpark()，它们底层都是调用的Unsafe的这两个方法。
+     */
+
+}
+
+
+
+class Person {
+    private int age;
+
+    public Person(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
+
+
